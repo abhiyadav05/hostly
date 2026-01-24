a3586c870531817c4991e94992679cd7ef523a50
abhiyadav05 <abhshekyadav95065@gmail.com>
2026-01-24 17:25:48 +0530

    create the restful api for the generating the url of the project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {ECSClient,RunTaskCommand} from '@aws-sdk/client-ecs';
 import dotenv from "dotenv";
+import { nanoid } from 'nanoid';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -17,11 +18,11 @@ const ecsClient = new ECSClient({
 })
 
 
-app.get('/build',(req,res)=>{
+app.post('/build',async (req,res)=>{
     const {gitUrl}= req.body;
+    const projectId = "rakesh";
 
-
-    const runTaskcmd=  new RunTaskCommand({
+    const runTaskcmd=   new RunTaskCommand({
         cluster: process.env.CLUSTER_ARN,
         taskDefinition: process.env.TASK_ARN,
         launchType: 'FARGATE',
@@ -41,9 +42,42 @@ app.get('/build',(req,res)=>{
                     name : 'GIT_REPOSITORY_URL',
                     value : gitUrl,
                     },
-                    
+                    {
+                        name : 'PROJECT_ID',
+                        value : projectId,
+                    },
+                    {
+                        name :'S3_BUCKET_NAME',
+                        value : process.env.S3_BUCKET_NAME,
+                    },
+                    {
+                        name : 'AWS_REGION',
+                        value : process.env.AWS_REGION,
+                    },
+                    {
+                        name : 'AWS_ACCESS_KEY_ID',
+                        value : process.env.AWS_ACCESS_KEY_ID,
+                    },
+                    {
+                        name : 'AWS_SECRET_ACCESS_KEY',
+                        value : process.env.AWS_SECRET_ACCESS_KEY,
+                    }      
             ]
             }]
         }
+    });
+
+    await ecsClient.send(runTaskcmd);
+
+    return res.json({
+        status : 'built',
+        data : {
+            projectId,
+            url : `http://${projectId}.localhost:3000`
+        }
     })
+})
+
+app.listen(PORT,()=>{
+  console.log(`app running on ${PORT}`);
 })
